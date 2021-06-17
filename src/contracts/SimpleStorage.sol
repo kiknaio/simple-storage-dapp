ccf0bb09a0f81db210a750707accc716742d1ac7
George Kiknadze <g.kiknadze@hotmail.com>
2021-06-17 17:20:05 +0400

    update deploy script and use memory keyword for newer version of Solidity

--- src/contracts/SimpleStorage.sol
+++ src/contracts/SimpleStorage.sol
@@ -3,11 +3,11 @@ pragma solidity ^0.5.0;
 contract SimpleStorage {
     string public simpleStorage = "Simple Storage. Change me";
 
-    function updateSimpleStorage(string _newContent) public {
+    function updateSimpleStorage(string memory _newContent) public {
         simpleStorage = _newContent;
     }
 
-    function getSimpleStorageValue() public view returns (string) {
+    function getSimpleStorageValue() public view returns (string memory) {
         return simpleStorage;
     }
 
